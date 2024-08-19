c9978c8ad551664047a23ee4c6f32522de988c41
genuino12 <105496939+genuino12@users.noreply.github.com>
2024-08-19 18:06:58 +0000

    teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 
 const host = '000.0.0.0'; //rede disponivel
-const porta = 4000; //porta
+const porta = 3000; //porta
 const app = express();
 
 app.use(express.static('./publico')); 
