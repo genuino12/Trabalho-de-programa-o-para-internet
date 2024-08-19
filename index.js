a03100f7fbbb3ef55ef1c58ec7f7e97b54daf3ed
genuino12 <105496939+genuino12@users.noreply.github.com>
2024-08-19 18:07:17 +0000

    teste

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const host = '000.0.0.0'; //rede disponivel
 const porta = 3000; //porta
 const app = express();
 
-app.use(express.static('./publico')); 
 
 app.listen(porta, host, () => {
 console.log(`Servidor estará rodando em http://${host}:${porta}`);
