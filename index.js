911edad7e0cd563f894448e37fde7c2384c1413c
genuino12 <105496939+genuino12@users.noreply.github.com>
2024-08-19 17:04:13 +0000

    atualização

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const host = '0.0.0.0'; //rede disponivel
 const porta = 3000; //porta
 const app = express();
 
-app.use(express.static('./publico'))
+app.use(express.static('./publico'));
 
 app.listen(porta, host, () => {
 console.log(`Servidor estará rodando em http://${host}:${porta}`);
