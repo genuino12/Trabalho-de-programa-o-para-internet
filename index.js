7fac70e7467e3223801437f0fdce324e97e9ea90
genuino12 <105496939+genuino12@users.noreply.github.com>
2024-08-19 18:00:57 +0000

    atualização

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 
 const host = '127.0.0.1'; //rede disponivel
-const porta = 3000; //porta
+const porta = 4000; //porta
 const app = express();
 
 app.use(express.static('./publico')); 
