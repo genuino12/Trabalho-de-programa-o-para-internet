2472bd9e642f1b6fee40ef9b7897be4b79e724b6
genuino12 <105496939+genuino12@users.noreply.github.com>
2024-08-20 17:23:44 +0000

    aa

--- Publico/script.js
+++ Publico/script.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    const form = document.getElementById('loginForm');
+    const emailInput = document.getElementById('email');
+    const senhaInput = document.getElementById('senha');
+    const emailError = document.getElementById('emailError');
+    const senhaError = document.getElementById('senhaError');
 
 
 
-    
 })
