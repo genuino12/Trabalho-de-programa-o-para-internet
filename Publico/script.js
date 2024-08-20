34b389a0e954362cb9a01118fde6428eb0d53f22
genuino12 <105496939+genuino12@users.noreply.github.com>
2024-08-20 18:12:32 +0000

    aaa

--- Publico/script.js
+++ Publico/script.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function(){
     const emailError = document.getElementById('emailError');
     const senhaError = document.getElementById('senhaError');
     const nomeInput = document.getElementById('nome');
+    const nomeError = document.getElementById('nomeError');
+    const telefoneInput = document.getElementById('phone');
+    const telefoneError = document.getElementById('telefoneError');
 
     form.addEventListener('submit', function(event){
     event.preventDefault();
@@ -14,10 +17,15 @@ document.addEventListener('DOMContentLoaded', function(){
     senhaError.textContent = '';
     nomeError.textContent = '';
 
+    //validação do email
     const email = emailInput.value.trim();
     if (!validateEmail(email)) {
         emailError.textContent = 'Por favor, insira um e-mail válido.';
         return;
+}
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 }
     const senha = senhaInput.value.trim();
     if (senha.length < 6) {
@@ -33,12 +41,10 @@ if (email === 'teste@teste.com' && senha === '123456') {
     senhaError.textContent = 'Email ou senha inválidos.';
 }
 });
-    function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
-
 const nome = nomeInput.value.trim();
-
+if (nome.length < 5) {
+    nomeError.textContent = 'O nome deve ter pelo menos 5 caracteres.';
+    return;
+}
 
 });
