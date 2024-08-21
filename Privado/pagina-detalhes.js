d616d626eba380d78f1f9ff5dbd711dd5d0211f5
genuino12 <105496939+genuino12@users.noreply.github.com>
2024-08-21 16:39:48 +0000

    Atualização

--- Privado/pagina-detalhes.js
+++ Privado/pagina-detalhes.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     function calculateTotal(eventSection) {
         const quantityInput = eventSection.querySelector('.ticket-quantity');
-        const ticketPrice = parseFloat(eventSection.querySelector('.ticket-price').textContent);
-        const totalPriceElement = eventSection.querySelector('.total-price');
+        const ticketPrice = parseFloat(eventSection.querySelector('.ticket-price').textContent.replace(',', '.'));
+        const totalPriceInput = eventSection.querySelector('.total-price');
 
-        const totalPrice = (quantityInput.value * ticketPrice).toFixed(2);
-        totalPriceElement.textContent = totalPrice; // Atualiza o valor exibido na página
+        // Verifica se quantityInput e totalPriceInput existem
+        if (quantityInput && totalPriceInput) {
+            const quantity = parseFloat(quantityInput.value);
+            if (!isNaN(quantity) && quantity > 0) {
+                const totalPrice = (quantity * ticketPrice).toFixed(2);
+                totalPriceInput.value = totalPrice.replace('.', ','); // Atualiza o valor exibido no campo de entrada
+            } else {
+                totalPriceInput.value = '00,00'; // Define um valor padrão se a quantidade for inválida
+            }
+        }
     }
 
     function showPurchaseMessage(eventSection) {
         const purchaseMessage = eventSection.querySelector('.purchase-message');
-        const totalPrice = eventSection.querySelector('.total-price').textContent;
+        const totalPrice = eventSection.querySelector('.total-price').value;
         const quantity = eventSection.querySelector('.ticket-quantity').value;
 
         purchaseMessage.textContent = `Compra realizada com sucesso! Você comprou ${quantity} ingresso(s) por um total de R$ ${totalPrice}.`;
