910cdca9674fe7fe40b8f51e4b064a6e9d3e872f
genuino12 <105496939+genuino12@users.noreply.github.com>
2024-08-21 16:02:21 +0000

    atualização

--- Privado/pagina-detalhes.js
+++ Privado/pagina-detalhes.js
@@ -1,38 +1,42 @@
+
 function calculateTotal(eventSection) {
     const quantityInput = eventSection.querySelector('.ticket-quantity');
     const ticketPrice = parseFloat(eventSection.querySelector('.ticket-price').textContent);
-    const totalPriceElement = eventSection.querySelector('.total-price');
+    const totalPriceInput = eventSection.querySelector('.total-price');
 
     const totalPrice = (quantityInput.value * ticketPrice).toFixed(2);
-    totalPriceElement.textContent = totalPrice; // Atualiza o valor exibido na página
+    totalPriceInput.value = totalPrice;
 }
+
 
 function showPurchaseMessage(eventSection) {
     const purchaseMessage = eventSection.querySelector('.purchase-message');
-    const totalPrice = eventSection.querySelector('.total-price').textContent;
+    const totalPrice = eventSection.querySelector('.total-price').value;
     const quantity = eventSection.querySelector('.ticket-quantity').value;
 
     purchaseMessage.textContent = `Compra realizada com sucesso! Você comprou ${quantity} ingresso(s) por um total de R$ ${totalPrice}.`;
     purchaseMessage.style.color = 'green'; 
+
 
     setTimeout(() => {
         purchaseMessage.textContent = '';
     }, 5000);
 }
 
+
 document.querySelectorAll('section[id^="event-"]').forEach(section => {
     const quantityInput = section.querySelector('.ticket-quantity');
     const purchaseButton = section.querySelector('button[id^="purchase-button"]');
-
     quantityInput.addEventListener('input', () => calculateTotal(section));
-    purchaseButton.addEventListener('click', () => {
-        const paymentMethod = section.querySelector('.payment-method').value;
 
+    purchaseButton.addEventListener('click', () => showPurchaseMessage(section));
+
+    document.getElementById('purchase-button').addEventListener('click', function() {
+        const paymentMethod = document.getElementById('payment-method').value;
+    
         if (paymentMethod === 'registered-card') {
-            showPurchaseMessage(section);
             alert("Pagamento realizado com o cartão final 1234.");
         } else if (paymentMethod === 'registered-card-2') {
-            showPurchaseMessage(section);
             alert("Pagamento realizado com o cartão final 5678.");
         } else {
             alert("Por favor, escolha um método de pagamento válido.");
@@ -40,4 +44,6 @@ document.querySelectorAll('section[id^="event-"]').forEach(section => {
     });
 
     calculateTotal(section);
+    
 });
+
